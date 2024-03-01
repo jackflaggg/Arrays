78fad8f6b3411625ec8e6546b37bedc0b5b12639
jackflaggg <rasul.khamzinnn@gmail.com>
2024-03-01 17:13:14 +0500

    11 task

---
+++ Find the Index.js
@@ -0,0 +1,16 @@
+// Create a function that takes an array and a string as arguments and returns the index of the string.
+
+// Examples
+// findIndex(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// findIndex(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// findIndex(["a", "g", "y", "d"], "d") ➞ 3
+
+// findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+
+function findIndex(arr, str) {
+	return arr.findIndex(elem => elem === str)
+}
+
+console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"))
